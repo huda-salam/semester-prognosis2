9dadcbc3bcf99416f064d3d0f4e0b5103a495927
dingin <bpkad.dingin@kedirikab.go.id>
2026-07-09 17:51:12 +0700

    add allowedHosts di vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,7 @@ export default defineConfig(() => {
       hmr: process.env.DISABLE_HMR !== 'true',
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      allowedHosts: ['localhost', 'bpkad.kedirikab.go.id']
     },
   };
 });
